45e7efe08e714938def7d9955f7b5aed6cb05fd9
Iyman Ahmed <iymanahmed8@gmail.com>
2021-01-17 16:35:18 +0500

    Add files via upload

---
+++ Classifier/Logistic Regression/Logistic_Regression_Model.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+dataset= pd.read_csv('Social_Network_Ads.csv')
+
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train,y_train)
+
+prediction = classifier.predict(X_test)
+
+print(np.concatenate((prediction.reshape(len(prediction),1),y_test.reshape(len(y_test), 1)),1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test,prediction)
+print(cm)
+print(accuracy_score(y_test,prediction))
+
+plt.scatter(X_train ,y_train)
+plt.plot()
